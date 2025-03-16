3311019fd17fecf210139795e06b6073ebf2ddd3
Agent P <paullhungdim@iisc.ac.in>
2025-03-16 16:56:14 +0530

    oops

--- parallel.py
+++ parallel.py
@@ -8,6 +8,6 @@ def run_random(run_id):
     os.system(f"python prob2.py --sr_no 24323 --run_id {run_id}")
 
 if __name__ == "__main__":
-    with Pool(5) as p:
+    with Pool(4) as p:
         p.map(run_active, range(1,6))  # Active runs
         p.map(run_random, range(1,6))  # Random runs
